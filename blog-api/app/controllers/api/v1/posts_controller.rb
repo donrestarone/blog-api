ffa4a0915c7443a1cf8b328315a5cca464e22e63
donrestarone <shashikejayatunge@gmail.com>
2018-06-18 17:01:47 -0400

    set routes, api version

---
+++ blog-api/app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::PostsController < ApplicationController
+	def index
+		render json: Post.all
+	end
+
+	def show
+		render json: Post.first
+	end
+
+	def create
+		render json: {response: 'create'}
+	end
+
+	def destroy
+		render json: {response: 'destroy'}
+	end
+
+	def update
+		render json: {response: 'update'}
+	end
+end
